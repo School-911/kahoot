07ef2b97b17112ff1a0546566277068e783ceca3
School-911 <nguyendangtruong2004thd@gmail.com>
2025-07-01 17:20:35 +0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.html",
+    "./public/index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
