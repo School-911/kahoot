8b7e2db4d26ef9c9654cd35f82a24153c2c8127f
School-911 <nguyendangtruong2004thd@gmail.com>
2025-07-01 15:20:40 +0700

    Update cauHoiRoutes.js

--- server/routes/cauHoiRoutes.js
+++ server/routes/cauHoiRoutes.js
@@ -1,13 +1,20 @@
+import express from 'express'
+import {
+  taoCauHoi,
+  layTatCaCauHoi,
+  layCauHoiTheoId,
+  layCauHoiNgauNhien,
+  capNhatCauHoi,
+  xoaCauHoi
+} from '../controllers/cauHoiController.js'
+
+const router = express.Router()
+
 router.post('/', taoCauHoi)
-
-router.get('/', layTatCaCauHoi)
-
-router.put('/:id', capNhatCauHoi)
-
-router.delete('/:id', xoaCauHoi)
-
 router.get('/ngau-nhien', layCauHoiNgauNhien)
-
+router.get('/', layTatCaCauHoi)
 router.get('/:id', layCauHoiTheoId)
+router.put('/:id', capNhatCauHoi)
+router.delete('/:id', xoaCauHoi)
 
 export default router
