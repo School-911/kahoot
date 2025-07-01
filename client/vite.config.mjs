ebb2954b46f57caffbd069c764b337978dd4a145
School-911 <nguyendangtruong2004thd@gmail.com>
2025-07-02 02:09:09 +0700

    Add files via upload

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,15 +1,16 @@
-// vite.config.js
+// client/vite.config.js
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/', // 👈 Thêm dòng này
   plugins: [vue()],
   server: {
     proxy: {
       '/api': {
         target: 'https://kahoot-4f1i.onrender.com',
         changeOrigin: true,
-        // Giữ nguyên /api trong đường dẫn (KHÔNG rewrite!)
+        // KHÔNG rewrite /api
       }
     }
   }
