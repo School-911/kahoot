cb5413397eec08f6eb45dd04526252aefcfef0f1
School-911 <nguyendangtruong2004thd@gmail.com>
2025-07-01 14:02:19 +0700

    Delete client directory

--- client/src/socket.js
+++
@@ -1,8 +0,0 @@
-// socket.js - stub
-import { io } from 'socket.io-client'
-
-// Khởi tạo kết nối socket đến server backend
-const socket = io(import.meta.env.VITE_SOCKET_URL) // Thay bằng domain khi deploy
-
-export default socket
-     
