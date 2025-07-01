01f546c5fca02f219f55f8999dbf161bbe43a7b0
School-911 <nguyendangtruong2004thd@gmail.com>
2025-07-01 19:40:06 +0700

    Add files via upload

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,16 +1,22 @@
 // client/src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import TaoCauHoi from '../pages/admin/taocauhoi.vue'
+import PlayGame from '../pages/QuestionDisplay.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/admin/taocauhoi' // Nếu muốn trang chủ tự động chuyển đến
+    redirect: '/QuestionDisplay' // Nếu muốn trang chủ tự động chuyển đến
   },
   {
     path: '/admin/taocauhoi',
     name: 'TaoCauHoi',
     component: TaoCauHoi
+  },
+  {
+    path: '/choi/',           // ✅ thêm route mới
+    name: 'QuestionDisplay',
+    component: QuestionDisplay
   }
 ]
 
