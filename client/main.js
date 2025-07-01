fa4648adae89c84d00e455a52a4009d43b84b4ca
School-911 <nguyendangtruong2004thd@gmail.com>
2025-07-01 17:27:50 +0700

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/tailwind.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).use(router).mount('#app')
