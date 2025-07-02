43d0eabdf35f9e943ce55c0b0ea4103923ce1ac0
School-911 <nguyendangtruong2004thd@gmail.com>
2025-07-02 20:41:28 +0700

    Add files via upload

--- server/routes/quizRoutes.js
+++ server/routes/quizRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createQuiz } from '../controllers/quizController.js'
+import { createQuiz, getRandomQuiz } from '../controllers/quizController.js'
 
 const router = express.Router()
 router.post('/quizzes', createQuiz)
